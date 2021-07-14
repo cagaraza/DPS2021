aa68cc5ecaa781fbf3e7bb77e71108b0614b32cd
christian.garaza <christian.garaza@blaize.com>
2021-07-14 20:54:39 +0800

    Initial commit of collaterals for DPS project 2021

---
+++ main.py
@@ -0,0 +1,10 @@
+### Initial commit for data exploration work of Monatszahlen Verkehrsunfälle Dataset
+### Attributed License of Dataset used in Project: Datenlizenz Deutschland Namensnennung 2.0
+
+import pandas as pd
+
+
+df = pd.read_csv ('210619monatszahlenjuni2021monatszahlen2106verkehrsunfaelle.csv')
+
+
+print(df.head)
